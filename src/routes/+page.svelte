e0a6a4492ec9b4cf88f851b27cbf1333c6d0ffba
Jesse <jesseliot@hotmail.com>
2025-03-31 17:03:49 +0100

    styling removals

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -22,35 +22,3 @@
 	</h2>
 </section>
 
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		font-family: 'Times New Roman', Times, serif;
-		font-size: 20vh;
-		outline: dashed;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>
