5e41771d1145fed2af8f3e31c3d7a1378049ab25
Jesse <jesseliot@hotmail.com>
2025-06-12 18:57:17 +0100

    portfolio page change

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,24 +2,47 @@
 </script>
 
 <svelte:head>
-	<title>Jesse's Portfolio Site</title>
-	<meta name="description" content="Svelte demo app" />
+  <title>Jesse's Portfolio Site</title>
+  <meta name="description" content="Svelte demo app" />
 </svelte:head>
 
 <section>
-	<h1>
-		<div class="welcome">
-			GET ONTO It
-		</div>
+  <h1>
+    <div class="welcome">Jesse Eliot</div>
+  </h1>
+  {#snippet linkBlock(name: string, link: string)}
+    <div class="linkContainer">
+        <a href={link}>{name}</a>
+    </div>
+  {/snippet}
+  {@render linkBlock('Kerjumble', '/kerjumble')}
+  {@render linkBlock('Typewriter', '/typewriter')}
 
-		to your new<br /> app
-	</h1>
-	<a href="/kerjumble">Kerjumble      </a>
-	<a href="/typewriter">Typewriter Clicky</a>
-
-	<h2>
-		Real Portfolio Page Now! <strong>Jesse Get To Work</strong>
-		<a href="/arithmetic">Zetamac</a>
-	</h2>
+  <h2>
+    Portfolio Page
+	<strong>Hire Jesse. Make him work.</strong>
+    <a href="/arithmetic">Zetamac</a>
+  </h2>
 </section>
 
+<style>
+	section {
+		background-image: repeating-radial-gradient(rgb(150, 30, 72), rgb(190, 10, 121));
+		font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+	}
+	.linkContainer {
+		background-color: #CBB5;
+		margin: 0.5rem 0;
+		padding: 0.7rem;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		border-radius: 5px;
+		font-size: 2rem;
+		backdrop-filter: blur(10px);
+	}
+	a {
+		color: black;
+		font-style: italic;
+	}
+</style>
